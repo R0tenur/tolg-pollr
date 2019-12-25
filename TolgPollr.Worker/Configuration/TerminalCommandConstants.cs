8c13f1af2919f968905ea4aef97669f227654325
R0tenur <himself@jenshunt.se>
2019-12-25 11:39:19 +0100

    Fix cool param

--- TolgPollr.Worker/Configuration/TerminalCommandConstants.cs
+++ TolgPollr.Worker/Configuration/TerminalCommandConstants.cs
@@ -5,6 +5,6 @@ namespace TolgPollr.Worker.Configuration
         public const string LircCommand = "irsend";
 
         public static readonly string[] LircHeatParameters = new[] { "send_once", "ac", "on" };
-        public static readonly string[] LircCoolParameters = new[] { "send_once", "ac", "on" };
+        public static readonly string[] LircCoolParameters = new[] { "send_once", "ac", "off" };
     }
 }
